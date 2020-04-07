bdffec39015116f5710ebb4e159001e46314def8
neshkovac <lneskovac@hotmail.com>
2020-04-07 12:43:25 +0200

    Added Cart table design along with some jQuery.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,54 @@
-window.onload = function(){
+window.onload = function() {
   articlesOnLoad();
+  cartOnLoad();
+
 };
 
 
 // variables
 
-$productsWrapper = $("#productsRow");
+$productsCollumn = $('#productsCollumn');
+$cartWrapper = $('#showCart');
 
 // end of variables
 
 
-function articlesOnLoad(){
-  $.get('data/articles.json',function(data){
-      populateArticles(data);
+function articlesOnLoad() {
+  $.get('data/articles.json', function(data) {
+    populateArticles(data);
   });
 }
 
-function populateArticles(data){
+function populateArticles(data) {
   var result = ``;
 
-  data.forEach( a => {
-    result+=`
-        <div class="col-md-4 text-center single-article-wrapper">
+  data.forEach(a => {
+    result += `
+        <div class="col-md-6 text-center single-article-wrapper">
           <h5 class="single-article-heading">${a.name}</h5>
-          <a class="btn btn-default" href="#" data-productid="${a.id}">Add to cart</a>
-        </div> // end of single article
+          <a class="btn btn-primary add-to-cart-btn" href="#" data-articleid="${a.id}">Add to cart</a>
+        </div> <!-- end of single article -->
     `
   });
 
-  $productsWrapper.html(result);
+  $productsCollumn.html(result);
+
+  $('.add-to-cart-btn').click(bindOnClick);
 }
+
+function bindOnClick(e) {
+  e.preventDefault();
+  console.log(e.target.dataset.articleid);
+}
+
+function cartOnLoad() {
+  $cartWrapper.click(function() {
+      $('#cartTable').toggle(200, function(){
+        if ($('#showCart').html().trim() == "Show cart") {
+          $('#showCart').html("Hide cart");
+        } else {
+          $('#showCart').html("Show cart");
+        }
+      })
+    })
+  };
